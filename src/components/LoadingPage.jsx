cb6309dd09b4d2df1101b00b87943d0097db2a6e
Amoghavarsha-Rao <amoghavarsharao99@gmail.com>
2026-03-25 21:32:04 -0500

    type correction

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAppContext } from '../context/AppContext';
 import { generateCards } from '../utils/generateCards';
-import './LoadingPage.css';
+import './loadingPage.css';
 
 export default function LoadingPage() {
   const navigate = useNavigate();
